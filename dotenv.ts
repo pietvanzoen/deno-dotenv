67978a66db77db32df3d11631308cf132e60bc87
pietvanzoen <hi@pietvanzoen.com>
2021-08-07 16:47:07 +0200

    feat: deprecate dotenv.ts

--- dotenv.ts
+++
@@ -1,5 +0,0 @@
-export * from "./mod.ts";
-
-console.warn(
-  "Warning: In a future version loading from 'https://deno.land/x/dotenv/dotenv.ts' will be deprecated in favour of 'https://deno.land/x/dotenv/mod.ts'.",
-);
